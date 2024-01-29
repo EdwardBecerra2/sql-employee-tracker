664624b23eb2dc18658d7a33e25a8bd2efcedb97
EdwardBecerra2 <146788503+EdwardBecerra2@users.noreply.github.com>
2024-01-28 18:37:13 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// importing package files (console.table no longer required)
+const inquirer = require("inquirer");
+const mysql = require("mysql2/promise");
+
+// Encryption for env file
+require("dotenv").config();
+
+// Dotenv variables
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
